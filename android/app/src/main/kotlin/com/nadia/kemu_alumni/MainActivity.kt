1e351a3c2faafb6d4a295b4434ca553b917b21b3
nadia70 <41780932+Iradukunda-Nadia@users.noreply.github.com>
2020-02-12 19:34:11 +0300

    messaging

--- android/app/src/main/kotlin/com/nadia/kemu_alumni/MainActivity.kt
+++ android/app/src/main/kotlin/com/nadia/kemu_alumni/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.nadia.kemu_alumni
 
 import android.os.Bundle
-
-import io.flutter.app.FlutterActivity
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        GeneratedPluginRegistrant.registerWith(this)
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
     }
 }
