c1b5e60d41ff9c48eecfcaacb7083a9816f6ab67
Iradukunda-Nadia <esthernadia70@gmail.com>
2020-04-01 11:11:13 +0300

    messaging

--- lib/newEvent.dart
+++ lib/newEvent.dart
@@ -1,7 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'mpesa/mpesa_flutter_plugin.dart';
 
 class newEvents extends StatefulWidget {
   @override
@@ -15,6 +17,94 @@ class _newEventsState extends State<newEvents> {
     return qn.documents;
 
   }
+  var number = TextEditingController();
+  var amount = TextEditingController();
+  String payment;
+  String title;
+  String eventdate;
+
+  Future<void> startCheckout({String userPhone, int amount}) async {
+    //Preferably expect 'dynamic', response type varies a lot!
+    dynamic transactionInitialisation;
+
+    //Better wrap in a try-catch for lots of reasons.
+    try {
+      //Run it
+      transactionInitialisation =
+      await MpesaFlutterPlugin.initializeMpesaSTKPush(
+          businessShortCode: "174379",
+          transactionType: TransactionType.CustomerPayBillOnline,
+
+          ///Place the amount here
+          amount: double.parse(amount.toString()),
+          partyA: userPhone,
+          partyB: "174379",
+          callBackURL: Uri.parse("https://sandbox.safaricom.co.ke/"),
+          accountReference: "KeMU AA",
+          phoneNumber: userPhone,
+          baseUri: Uri.parse("https://sandbox.safaricom.co.ke/"),
+          transactionDesc: "purchase",
+          passKey:
+          "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919");
+
+      print("TRANSACTION RESULT: " + transactionInitialisation.toString());
+
+      await FirebaseAuth.instance.currentUser()
+
+          .then((user)=> Firestore.instance
+          .collection("receipt")
+          .document()
+          .setData({
+        "uid": user.uid,
+        "email": user.email,
+        "status": "KeMU AA ccontribution",
+        "event": title,
+        "amount": payment,
+        "date" : DateFormat(' dd MMM yyyy').format(DateTime.now()),
+      }));
+
+      FirebaseUser user = await FirebaseAuth.instance.currentUser();
+      await Firestore.instance
+          .collection("rsvp")
+          .add({
+        "event": title,
+        "date": eventdate,
+        "status": "RSVP",
+        "name": user.email,
+      }).then((result) =>
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+// return object of type Dialog
+              return AlertDialog(
+                title: Text("Rsvp Confirmed!"),
+                content: new Text("THANKYOU FOR YOUR CONTRIBUTION."),
+                actions: <Widget>[
+// usually buttons at the bottom of the dialog
+
+                  new FlatButton(
+                    child: new Text("CLOSE"),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          )
+      );
+
+
+      /*Update your db with the init data received from initialization response,
+      * Remaining bit will be sent via callback url*/
+      return transactionInitialisation;
+    } catch (e) {
+      //For now, console might be useful
+      print("CAUGHT EXCEPTION: " + e.toString());
+      return Text(e.toString());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -62,9 +152,43 @@ class _newEventsState extends State<newEvents> {
                                             Text(snapshot.data[index].data["title"], style: TextStyle(fontSize: 24.0, fontFamily: "Raleway",color: Colors.white)),
                                             Text(snapshot.data[index].data["date"], style: TextStyle(fontSize: 12.0, fontFamily: "Raleway")),
                                             Container(
-                                              width: MediaQuery.of(context).size.width * .3,
                                               height: 18,
                                               child: RaisedButton(
+                                                onPressed: () {
+                                                  showCupertinoDialog(
+                                                      context: context,
+                                                      builder: (BuildContext context) =>
+                                                          CupertinoActionSheet(
+                                                            title: Text(
+                                                                "Enter your mpesa number in the format defined"),
+                                                            message: Column(
+                                                              children: <Widget>[
+                                                                CupertinoTextField(
+                                                                  controller: number,
+                                                                  placeholder: '2547XXXXXXXX',
+                                                                ),
+                                                              ],
+                                                            ),
+                                                            actions: <Widget>[
+                                                              CupertinoButton(
+                                                                child: Text("Proceed"),
+                                                                onPressed: () {
+                                                                  setState(() {
+                                                                    payment = snapshot.data[index].data["amount"];
+                                                                    title = snapshot.data[index].data["title"];
+                                                                    eventdate = snapshot.data[index].data["date"];
+                                                                  });
+                                                                  startCheckout(
+                                                                      userPhone: number.text,
+                                                                      amount: 1);
+                                                                  Navigator.of(context).pop();
+                                                                },
+                                                              )
+                                                            ],
+                                                          ));
+
+                                                },
+
                                                 child: Text(
                                                   "RSVP- KSH:${snapshot.data[index].data["amount"]}",
                                                   style: Theme.of(context)
@@ -75,77 +199,6 @@ class _newEventsState extends State<newEvents> {
                                                 ),
                                                 color: Colors.pink[900],
 
-                                                onPressed: () async {
-                                                  showDialog(
-                                                    context: context,
-                                                    builder: (BuildContext context) {
-                                                      // return object of type Dialog
-                                                      return AlertDialog(
-                                                        content: new Text("Pay now to RSVP"),
-                                                        actions: <Widget>[
-                                                          // usually buttons at the bottom of the dialog
-                                                          new FlatButton(
-                                                            child: new Text("YES"),
-                                                            onPressed: () async {
-                                                              await FirebaseAuth.instance.currentUser()
-
-                                                                  .then((user)=> Firestore.instance
-                                                                  .collection("receipt")
-                                                                  .document()
-                                                                  .setData({
-                                                                "uid": user.uid,
-                                                                "email": user.email,
-                                                                "status": "KeMU AA ccontribution",
-                                                                "event": snapshot.data[index].data["title"],
-                                                                "amount": snapshot.data[index].data["amount"],
-                                                                "date" : DateFormat(' dd MMM yyyy').format(DateTime.now()),
-                                                              }));
-
-                                                              FirebaseUser user = await FirebaseAuth.instance.currentUser();
-                                                              await Firestore.instance
-                                                                  .collection("rsvp")
-                                                                  .add({
-                                                                "event": snapshot.data[index].data["title"],
-                                                                "date": snapshot.data[index].data["date"],
-                                                                "status": "RSVP",
-                                                                "name": user.email,
-                                                              }).then((result) =>
-                                                                  showDialog(
-                                                                    context: context,
-                                                                    builder: (BuildContext context) {
-                                                                      // return object of type Dialog
-                                                                      return AlertDialog(
-                                                                        content: new Text("THANKYOU FOR YOUR CONTRIBUTION"),
-                                                                        actions: <Widget>[
-                                                                          // usually buttons at the bottom of the dialog
-
-                                                                          new FlatButton(
-                                                                            child: new Text("CLOSE"),
-                                                                            onPressed: () {
-                                                                              Navigator.of(context).pop();
-                                                                            },
-                                                                          ),
-                                                                        ],
-                                                                      );
-                                                                    },
-                                                                  )
-                                                              );
-
-                                                              Navigator.of(context).pop();
-                                                            },
-                                                          ),
-                                                          new FlatButton(
-                                                            child: new Text("NO"),
-                                                            onPressed: () {
-                                                              Navigator.of(context).pop();
-                                                            },
-                                                          ),
-                                                        ],
-                                                      );
-                                                    },
-                                                  );
-
-                                                },
                                               ),
                                             ),
                                             SizedBox(
